0b2bd5a6eb3cbe8873e979c514e929eaf17d163c
mdhasanshuvo <mdhasanshuvo220@gmail.com>
2024-12-05 09:29:26 +0600

    Navbar added

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello world!</div>,
-  },
-]);
+import { RouterProvider } from 'react-router-dom';
+import AppRoutes from './routes/AppRoutes.jsx';
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={AppRoutes} />
   </StrictMode>,
 )
---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from '../components/Navbar';
+import Navbar from '../components/Navbar';
+
+const Home = () => {
+    return (
+        <div>
+            {/* header section */}
+            <header>
+                <Navbar></Navbar>
+            </header>
+
+        </div>
+    );
+};
+
+export default Home;
